10e16eb917d2f9202ab2f4e62e2e67d6832a123d
Twilly0 <Twilly0@61a55b36-2ee7-8d4d-74e8-20864f2fa4dc>
2010-06-22 21:35:34 +0000

    Made Main do something.
    git-svn-id: https://mvhsbandinventory.googlecode.com/svn/trunk@25 61a55b36-2ee7-8d4d-74e8-20864f2fa4dc

--- Application/src/mvhsbandinventory/Main.java
+++ Application/src/mvhsbandinventory/Main.java
@@ -2,20 +2,29 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mvhsbandinventory;
+
+import javax.swing.JFrame;
 
 /**
  *
- * @author jonathan
+ * @author nicholson
  */
-public class Main {
+public class Main
+{
+    public static JFrame window = new JFrame();
+    public static Display panel = new Display();
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        window.add(panel);
+        window.setSize(800,800);
+        window.setTitle("MVHS - Band Inventory");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+        panel.repaint();
     }
-
 }
