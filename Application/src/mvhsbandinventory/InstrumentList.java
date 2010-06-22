a6b5d4c3c75aeaf091fc7b88d508aab8b40bde74
speedbreeze <speedbreeze@61a55b36-2ee7-8d4d-74e8-20864f2fa4dc>
2010-06-22 20:41:19 +0000

    Adding placeholder InstrumentList class.
    git-svn-id: https://mvhsbandinventory.googlecode.com/svn/trunk@17 61a55b36-2ee7-8d4d-74e8-20864f2fa4dc

---
+++ Application/src/mvhsbandinventory/InstrumentList.java
@@ -0,0 +1,38 @@
+package mvhsbandinventory;
+
+/**
+ *
+ * @author jonathan
+ */
+public class InstrumentList
+{
+    public InstrumentList (String path) {
+        
+    }
+
+    public void add (Instrument instrument)
+    {
+
+    }
+
+    public void sort (String key)
+    {
+
+    }
+
+    public void delete (Instrument instrument)
+    {
+
+    }
+
+    public Instrument[] selectList (String key, Object value)
+    {
+        // Netbeans, please stop whining about my code.
+        return null;
+    }
+
+    public String exportToExcel (Instrument[] list) {
+        // Netbeans, please stop whining about my code.
+        return null;
+    }
+}
