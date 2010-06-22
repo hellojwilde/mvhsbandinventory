1671d7ee1b09cc7fa30fbe2b0669a3d476cdeb85
speedbreeze <jonathan+github@speedbreeze.com>
2010-06-22 20:50:25 +0000

    Filling in code for add and remove methods.

--- Application/src/mvhsbandinventory/InstrumentList.java
+++ Application/src/mvhsbandinventory/InstrumentList.java
@@ -1,18 +1,22 @@
 package mvhsbandinventory;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jonathan
  */
 public class InstrumentList
 {
+    private ArrayList list = new ArrayList();
+
     public InstrumentList (String path) {
-        
+        // TODO: load all items in folder at path into `list`
     }
 
     public void add (Instrument instrument)
     {
-
+        list.add(instrument);
     }
 
     public void sort (String key)
@@ -22,7 +26,7 @@ public class InstrumentList
 
     public void delete (Instrument instrument)
     {
-
+        list.remove(instrument);
     }
 
     public Instrument[] selectList (String key, Object value)
@@ -31,7 +35,7 @@ public class InstrumentList
         return null;
     }
 
-    public String exportToExcel (Instrument[] list) {
+    public String exportToExcel (Instrument[] instruments) {
         // Netbeans, please stop whining about my code.
         return null;
     }
