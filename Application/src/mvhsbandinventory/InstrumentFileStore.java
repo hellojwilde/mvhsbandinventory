808d4d5a5bb9b4ad17fc180340e7f4bd4e5fe1bd
speedbreeze <speedbreeze@61a55b36-2ee7-8d4d-74e8-20864f2fa4dc>
2010-06-23 21:39:43 +0000

    Created basic subclass of InstrumentStore.  This contains basic code to load a reference to the folder containing the instrument store and check if specific instruments exist.  A private file method simplifies getting a File object for the Instrument.
    git-svn-id: https://mvhsbandinventory.googlecode.com/svn/trunk@43 61a55b36-2ee7-8d4d-74e8-20864f2fa4dc

--- Application/src/mvhsbandinventory/InstrumentFileStore.java
+++ Application/src/mvhsbandinventory/InstrumentFileStore.java
@@ -1,14 +1,65 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package mvhsbandinventory;
+
+import java.io.File;
 
 /**
  *
  * @author jonathan
  */
-public class InstrumentFileStore {
-
+public class InstrumentFileStore extends InstrumentStore 
+{
+	private File directory;
+	
+	/**
+	 * Constructs a new InstrumentFileStore object for storing Instrument 
+	 * information on the disk.
+	 * @param path
+	 * @throws Error
+	 */
+	public void InstrumentFileStore (String path) throws Error
+	{
+		directory = new File(path);
+		
+		if (!directory.exists()) 
+		{
+			throw new Error("File store path does not exist.");
+		}
+		
+		if (!directory.isDirectory()) 
+		{
+			throw new Error("File story path must be a directory; the current" +
+							" path is not a directory");
+		}
+	}
+	
+	/**
+	 * Generates a File object that should reference the specified instrument
+	 * in the data store.
+	 * @param instrument
+	 * @return file that should reference the specified instrument
+	 */
+	private File file (Instrument instrument)
+	{
+		String name = instrument.get("name");
+		String brand = instrument.get("brand");
+		String serial = instrument.get("serial");
+		
+		// Generate the CSV file name
+		String fileName = name + "-" + brand + "-" + serial + ".csv";
+		
+		return new File(directory, fileName);
+	}
+	
+	/**
+	 * Determines whether the specified instrument is already stored in the 
+	 * data store.
+	 * @param instrument
+	 * @return a boolean value determining whether the instrument is in the 
+	 * data store; true indicates that the instrument is in the store and false
+	 * indicates that it doesn't exist in the data store
+	 */
+	public boolean exists (Instrument instrument)
+	{
+		return file(instrument).exists();
+	}
 }
