b89824b0cc139e51bb83b80b45c7c4f4aca40c43
speedbreeze <jonathan+github@speedbreeze.com>
2010-06-23 20:41:03 +0000

    Defined an abstract class (InstrumentStore) to define the requirements for a data store to hold the instruments.  The InstrumentFileStore class will implement this.

--- Application/src/mvhsbandinventory/InstrumentStore.java
+++ Application/src/mvhsbandinventory/InstrumentStore.java
@@ -1,14 +1,34 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package mvhsbandinventory;
 
 /**
  *
  * @author jonathan
  */
-public class InstrumentStore {
+public abstract class InstrumentStore
+{
+    /**
+     * Adds a new instrument to the store; serializes all of the data and
+     * writes the data to the disk.
+     * @param instrument
+     */
+    public abstract void add (Instrument instrument);
 
+    /**
+     * Updates an existing instrument in the store; serializes all of the data
+     * in the instrument parameter and writes the data to the disk.
+     * @param instrument
+     */
+    public abstract void update (Instrument instrument);
+
+    /**
+     * Deletes the instrument from the store permenantly.
+     * @param instrument
+     */
+    public abstract void delete (Instrument instrument);
+
+    /**
+     * Loads all of the instruments from the store.
+     * @return an array of all of the parsed instruments in the store
+     */
+    public abstract Instrument[] load ();
 }
