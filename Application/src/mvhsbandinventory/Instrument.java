2065b06c00ad54a3f4056d02d3505519a9604ca2
speedbreeze <speedbreeze@gmail.com>
2010-07-07 15:16:36 -0700

    Moved history storage out of the HashMap of properties.

--- Application/src/mvhsbandinventory/Instrument.java
+++ Application/src/mvhsbandinventory/Instrument.java
@@ -1,5 +1,6 @@
 package mvhsbandinventory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -14,13 +15,14 @@ import java.util.Map;
 public class Instrument
 {
     private Map<String,String> properties = new HashMap<String,String>();
+    private List<String> history = new ArrayList<String>();
 
     public static String[] attributes = 
 		{ 
 			"Name", "Brand", "Serial", "Rank", "Value", "Status", "Notes", 
 			"Ligature", "Mouthpiece", "MouthpieceCap", "Bow", "NeckStrap",
             "Renter", "SchoolYear", "DateOut", "Fee", "Period", "Other",
-            "Contract", "History"
+            "Contract"
         };
 
 
@@ -77,5 +79,20 @@ public class Instrument
     public String get (String attribute)
     {
         return properties.get(attribute);
+    }
+
+    public List<String> getHistory ()
+    {
+        return history;
+    }
+
+    public void setHistory (List<String> history)
+    {
+        this.history = history;
+    }
+
+    public void addHistory (String line)
+    {
+        history.add(line);
     }
 }
