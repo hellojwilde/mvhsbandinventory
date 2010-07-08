8c7f6064e008c608db41ae77381af3a7a2df343d
speedbreeze <jonathan+github@speedbreeze.com>
2010-07-07 22:37:35 -0700

    Removing unused imports from InstrumentFileStore.

--- Application/src/mvhsbandinventory/InstrumentFileStore.java
+++ Application/src/mvhsbandinventory/InstrumentFileStore.java
@@ -2,13 +2,11 @@ package mvhsbandinventory;
 
 import au.com.bytecode.opencsv.CSVWriter;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
